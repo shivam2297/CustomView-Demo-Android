15abcf661f9b5edc8cd69f43ae61e468aa2a16ad
Shivam Bansal <shivam2297@gmail.com>
2019-04-08 10:37:17 +0530

    Added app center sdk. Added signing configs.

--- app/src/main/java/com/example/shivam/customviewsdemo/MainActivity.java
+++ app/src/main/java/com/example/shivam/customviewsdemo/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.shivam.customviewsdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(),
+                getResources().getString(R.string.app_center_secret),
+                Analytics.class, Crashes.class);
     }
 }
